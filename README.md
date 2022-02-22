e831a201d198b4dad0c2bb3fe0cdc5b1944f4f85
ryong9rrr <tkddbs1644@gmail.com>
2022-02-22 23:37:47 +0900

    feat: CRUD 기능 구현

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 # Vanilla To do list server
 
-## API
-
-...
-
-</br>
-
 <table>
     <thead>
         <tr>
@@ -29,3 +23,51 @@
         </tr>
     </tbody>
 </table>
+
+</br>
+
+# Getting started
+
+1. git clone this repository.
+
+2. in your cloned folder, install dependencies
+
+`npm install`
+
+3. run
+
+`npm start`
+
+</br>
+
+# API
+
+## 모든 할 일 목록을 가져온다.
+
+```
+GET /items
+```
+
+## 할 일을 삭제한다.
+
+```
+DELETE /items/:category/:id
+```
+
+## 새로운 할 일을 입력한다.
+
+```
+POST /items
+```
+
+## 이름을 수정한다.
+
+```
+PUT /items/:id
+```
+
+## 할 일을 이동시킨다.
+
+```
+PUT /items/:category/:id
+```
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const morgan = require("morgan");
 const express = require("express");
+const { nanoid } = require("nanoid");
 const app = express();
 const PORT = 3000;
 
@@ -7,18 +8,101 @@ const html = `
     <h1>투두리스트 서버입니다.</h1>
 `;
 
-const store = {
-  todos: [],
-  doings: [],
-  dones: [],
-};
+class Item {
+  constructor(text, id = nanoid()) {
+    const createdTime = Date.now();
+    this.id = id;
+    this.category = "todo";
+    this.text = text;
+    this.createdAt = Number(createdTime);
+    this.updatedAt = Number(createdTime);
+  }
+}
+
+const items = [
+  new Item("자바스크립트 공부하기", "1"),
+  new Item("서버만들기", "2"),
+];
 
 app.use(morgan("dev"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get("/", function (req, res) {
-  res.send(html);
+app.get("/", (req, res) => {
+  return res.send(html);
+});
+
+app.get("/items", (req, res) => {
+  return res.status(200).json(items).end();
+});
+
+app.delete("/items/:category/:id", async (req, res) => {
+  const prevLength = items.length;
+  const { category, id } = req.params;
+  if (category !== "todo" || category !== "doing" || category !== "done") {
+    return res.status(400).end();
+  }
+
+  const valid = (item) => item.category === category && item.id === id;
+  const targetIndex = items.findIndex(valid);
+
+  if (targetIndex < 0) {
+    return res.status(400).send("존재하지 않는 id에요.").end();
+  }
+
+  items.splice(targetIndex, 1);
+  if (prevLength === items.length - 1) {
+    return res.status(204).end();
+  }
+
+  // 로직이 잘못되었다.
+  return res.status(403).end();
+});
+
+app.post("/items", (req, res) => {
+  if (!req.body.text) {
+    return res.status(400).end();
+  }
+  const newItem = new Item(req.body.text);
+  items.push(newItem);
+  return res.status(201).json(newItem).end();
+});
+
+app.put("/items/:id", (req, res) => {
+  const { id } = req.params;
+  const newText = req.body.text;
+  const targetIndex = items.findIndex((item) => item.id === id);
+
+  if (targetIndex < 0) {
+    return res.status(400).end();
+  }
+
+  items[targetIndex].text = newText;
+  return res.json(items[targetIndex]).end();
+});
+
+app.put("/items/:category/:id", (req, res) => {
+  const { category, id } = req.params;
+  if (!(category === "todo" || category === "doing")) {
+    return res.status(400).end();
+  }
+  const targetIndex = items.findIndex((item) => item.id === id);
+  if (targetIndex < 0) {
+    return res.status(404).end();
+  }
+  items[targetIndex].updatedAt = Date.now();
+  if (category === "todo") {
+    items[targetIndex].category = "doing";
+  } else if (category === "doing") {
+    items[targetIndex].category = "done";
+  }
+  const result = items[targetIndex];
+  items.sort((a, b) => a.updatedAt - b.updatedAt);
+  return res.json(result);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
+
+module.exports = app;
